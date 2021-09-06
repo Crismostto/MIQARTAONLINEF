99e91013f471a8c4b0dd796f12ad1df65cdef6e5
crismostto <crismstt@gmail.com>
2021-09-06 19:06:07 -0300

    Articulos creados

---
+++ src/components/Api/Web.vue
@@ -0,0 +1,82 @@
+<script>
+export default {
+    
+    methods:{
+        traerDatosPorId(ruta, id){
+            let direccion= "http://127.0.0.1:8000/" + ruta + "/" + id;
+            console.log("Traer datos por id ",direccion)
+            return fetch(direccion,
+            {
+                method: 'GET',
+                headers:{
+                    'content-type': 'application/json'
+                }
+            })
+            .then (respuesta =>{
+                return respuesta.json()
+            })
+        },
+
+        obtenerDatos(ruta){
+            let direccion = "http://127.0.0.1:8000/" + ruta
+            console.log("obtener datos",direccion)
+             return fetch(direccion,
+            {
+                method: 'GET',
+                headers:{
+                    'Content-type': 'application/json'
+                }
+            })
+            .then(respuesta =>{
+                return respuesta.json()
+            })
+        },
+        insertarDatosApi(ruta, agregarDatos){
+            let direccion ="http://127.0.0.1:8000/" +  ruta
+            console.log("insertar datos api",direccion)
+             return fetch(direccion,
+            {
+                method: 'POST',
+                headers:{
+                    'Content-type': 'application/json'
+                },
+                body: JSON.stringify(agregarDatos)
+            })
+            .then(respuesta =>{
+                return respuesta.json()
+            })
+        },EditarDatosApi(ruta, id, agregarDatos){
+            let direccion ="http://127.0.0.1:8000/" + ruta+ "/" +id
+            console.log("editar datos api",direccion)
+             return fetch(direccion,
+            {
+                method: 'PUT',
+                headers:{
+                    'Content-type': 'application/json'
+                },
+                body: JSON.stringify(agregarDatos)
+            })
+             .then(respuesta =>{
+                return respuesta.json()
+            })
+            
+
+        },EliminarDatosApi(ruta, id){
+          let direccion ="http://127.0.0.1:8000/" + ruta + "/" + id
+          console.log("eliminar Datos api",direccion)
+            return fetch(direccion,
+            {
+                method: 'DELETE',
+                headers:{
+                    'Content-type': 'application/json'
+                },
+               
+               
+            }) 
+            //  .catch(let error => {
+            //      console.log("error");
+            //  })
+        }
+    }
+}
+</script>
--- src/views/Articulo.vue
+++ src/views/Articulo.vue
@@ -1,5 +1,46 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <ArticulosABM v-if="verabmarticulos"/>
+    <span>Ingresar</span>
+       
+       <button v-on:click="ABMARTICULOS('agregar',articulos.id)">Agregar</button>
+       <ul v-for="(articulos, index) in datos" :key="index">
+           <li>
+               {{articulos.title}}
+           </li>
+           <button v-on:click="ABMARTICULOS('editar',articulos.id)">Editar</button>
+           <button v-on:click="ABMARTICULOS('eliminar',articulos.id)">Eliminar</button>
+       </ul>
   </div>
 </template>
+
+<script>
+
+import ArticulosABM from '@/components/Articulo/ArticulosABM.vue'
+import Web from  '@/components/Web.vue'
+
+export default {
+  components: {
+    ArticulosABM,
+    Web
+  },
+
+   data(){
+        return {
+         datos:[],
+         verabmarticulos:true,
+        }
+    },
+
+      methods: {
+        ABMARTICULOS(accion,id=0){
+          if (accion=='editar'){
+             this.verabmrubro= true;
+          }
+         
+        },
+
+    },
+
+}
+</script>
