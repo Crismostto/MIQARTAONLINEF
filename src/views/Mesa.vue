4d9a1568b8d2057dd460470bb04a1b0e5296cd82
Mariano Sack <marianoasack@gmail.com>
2021-09-22 18:57:13 -0300

    pedido y mesas

--- src/views/Mesa.vue
+++ src/views/Mesa.vue
@@ -1,21 +1,44 @@
 <template>
-  <div class="home">
-    <h1>Bienvenidos a la seccion de mesas</h1> 
-    <div>
-      <MesaINFO/> 
-    </div>  
-
+  <div>
+    <table>
+      <tr>
+        <th>NUMERO MESA</th>
+        <th>ESTADO</th>
+        <th>FECHA APERTURA</th>
+      </tr>
+      <tr v-for="(mesas, index) in datos" :key="index">
+        <button>
+          <td>{{ mesas.id }}</td>
+          <td>{{ mesas.estado }}</td>
+          <td>{{ mesas.fechaApertura }}</td>
+        </button>
+        <mesaPedido></mesaPedido>
+      </tr>
+    </table>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import MesaINFO from '@/components/MesaINFO.vue'
-
+import Web from "@/components/Api/Web.vue";
+import mesaPedido from "@/views/MesaPedido.vue"
 export default {
-  name: 'Home',
-  components: {
-    MesaINFO
-  }
-}
+  mixins: [Web],
+  components:{mesaPedido},
+  data() {
+    return {
+      datos: [],
+    };
+  },
+  created() {
+    this.traerDatos();
+  },
+  methods: {
+    traerDatos() {
+      this.obtenerDatos("mesas").then((respuesta) => {
+        this.datos = respuesta;
+      });
+    },
+  },
+};
 </script>
---
+++ src/views/MesaPedido.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <table>
+      <tr>
+        <th>pedido numero</th>
+        <th>Cantidad</th>
+        <th>precio</th>
+        <th>articulo</th>
+      </tr>
+      <tr v-for="(mesaPedido, index) in datos" :key="index">
+        
+          <td>{{mesaPedido.id}}</td>
+          <td>{{ mesaPedido.nombre }}</td>
+          <td>{{ mesaPedido.cantidad }}</td>
+          <td>{{ mesaPedido.precio }}</td>
+      
+      </tr>
+    </table>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Api from "@/components/Api/Api.vue";
+
+export default {
+  mixins: [Api],
+  data() {
+    return {
+      datos: [],
+    };
+  },
+  created() {
+    this.traerDatos();
+  },
+  methods: {
+    traerDatos() {
+      this.obtenerDatos("pedidos").then((respuesta) => {
+        this.datos = respuesta;
+      });
+    },
+  },
+};
+</script>
