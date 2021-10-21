26d48deac82e56ba7eb6db1258b3e35547a6663c
crisMostto <crismstt@gmail.com>
2021-10-21 17:10:40 -0300

    vista usuario

--- src/views/Usuario/Usuario.vue
+++ src/views/Usuario/Usuario.vue
@@ -2,8 +2,19 @@
   <div class="usuario"> 
     <div class="navbar-container">
       <div class="navbar-content">
-        <div class="icons">
-         <img src="..\src\assets\comida-user.png"/>  
+        <div class="icons">            
+            <router-link to="/usuario">
+            <img src="..\..\..\src\assets\comida-user.png" alt="" class="bx-sm" />
+            </router-link> 
+            
+            <router-link to="/usuario/pedido">
+            <img src="..\..\..\src\assets\comida-user.png" alt="" />
+            </router-link>
+
+             <router-link to="/usuario/pedido">
+            <img src="..\..\..\src\assets\comida-user.png" alt="" />
+            </router-link>
+
         </div>
       </div>
     </div>
@@ -13,9 +24,27 @@
 <style scoped>
 .navbar-container{
  width: 100%;
- background-color: red;
+ 
+ position:fixed;
+ bottom:0;
 }
 
+.navbar-content{
+ 
+}
+
+a {
+  color: rgba(163, 163, 163, 163);
+}
+
+a:hover{
+  background-color: rgba(255, 131, 62);
+}
+.icons{
+  padding:20px;
+  display: flex;
+  justify-content: space-around;
+}
 .usuario{
   text-align: center;
   margin: 0px;
