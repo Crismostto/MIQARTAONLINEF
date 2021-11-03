94534f2f299adeb3e2d284352b55805cb0b79a24
Mariano Sack <marianoasack@gmail.com>
2021-11-03 02:50:24 -0300

    a

--- src/views/HistoricoPedidos.vue
+++ src/views/HistoricoPedidos.vue
@@ -1,47 +1,128 @@
 <template>
-  <div>
-    <table>
-      <tr>
-        
-        <th>Articulo</th>
-        <th>Cantidad</th>
-        <th>Precio</th>
-        <th>Total</th>
-      </tr>
-      <tr v-for="(mesaPedido, index) in datos" :key="index">
-        
+  <div class="mesas">
+    <div class="header">
+      <h1 id="h1">Historico de los pedidos</h1>
+    </div>
+    <hr />
+        <div>
+        <input type="datetime-local" v-model="fechaUno">
+        <input type="datetime-local" v-model="fechaDos">
+        <button @click="filtrarHistoricos"> filtrar </button>
+        <button @click="todosLosHistoricos"> All </button>
+    </div>
+    <hr />
+    <div class="body">
+     
+      <!-- Prueba de tabla en Vue -->
+      <b-table
+        responsive
+        bordered
+        fixed
+        striped
+        hover
+        :items="fecha"
+        :fields="fields"
+        :per-page="perPage"
+        :current-page="currentPage"
+      >
+        <template #cell(Accion)="row">
+          <b-button size="sm" @click="row.toggleDetails" class="mr-2">
+            {{ row.detailsShowing ? "Ocultar" : "Mostrar" }} Detalles
+          </b-button>
+        </template>
 
-          <td>{{mesaPedido.nombre }}</td>
-          <td>{{mesaPedido.cantidad }}</td>
-          <td>{{mesaPedido.precio }}</td>
-          <td>{{mesaPedido.Total }}</td>
-      
-      </tr>
-    </table>
+        <template #row-details="row">
+          <b-card>
+            <b-row class="mb-2 hijo">
+              <HistoricoPedidos :id_Pedido="row.item.id"></HistoricoPedidos>
+            </b-row>
+
+            <b-button size="sm" @click="row.toggleDetails"
+              >Ocultar Detalles</b-button
+            >
+          </b-card>
+        </template>
+      </b-table>
+    
+      <b-pagination
+      v-model="currentPage"
+      :total-rows="rows"
+      :per-page="perPage"
+      aria-controls="my-table"
+    ></b-pagination>
+
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import Api from "@/components/Api/Api.vue";
-
+import HistoricoPedidos from "@/views/HistoricoPedidos.vue";
 export default {
   mixins: [Api],
-  props:["id_Pedido"],
+  components: { HistoricoPedidos },
   data() {
     return {
+      fields: [
+                { key: "id" , label:"Id_Pedido" , thStyle: { backgroundColor: 'rgb(209,231,221)'}},
+        { key: "mesa_id" , label:"Mesa" , thStyle: { backgroundColor: 'rgb(209,231,221)'}},
+        { key: "fecha_apertura" , thStyle: { backgroundColor: 'rgb(209,231,221)'}},
+        { key: "fecha_cierre" , thStyle: { backgroundColor: 'rgb(209,231,221)'}},
+        { key: "Accion", label: "Accion" , thStyle: { backgroundColor: 'rgb(209,231,221)'}},
+
+        
+      ],
       datos: [],
+      
+      fechaUno:0,
+      fecha:[],
+      fechaDos:0,
+      currentPage: 1,
+      perPage:10,
+      
     };
   },
   created() {
     this.traerDatos();
   },
+  computed: {
+      rows() {
+        return this.datos.length
+      }
+    },
   methods: {
     traerDatos() {
-      this.traerDatosPorId("historico/pedidos", this.id_Pedido).then((respuesta) => {
+      this.ObtenerDatos("historico/mesas").then((respuesta) => {
         this.datos = respuesta;
       });
     },
+    
+    
+    filtrarHistoricos(){
+    
+    this.fecha= this.datos.filter(n => n.fecha_cierre > this.fechaUno && n.fecha_cierre < this.fechaDos);
+    
+    },
+    todosLosHistoricos(){
+      this.fecha = this.datos;
+    }
   },
 };
 </script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.hijo {
+  display: flex;
+  text-align: center;
+}
+.header {
+  text-align: center;
+}
+
+#h1 {
+  padding-top: 20px;
+}
+
+</style>
